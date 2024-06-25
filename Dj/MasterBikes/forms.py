b98b088237d3fb886d4a372b210e40f5b55f3065
relaxcode425 <jamesvergaracoronado@gmail.com>
2024-06-24 21:36:51 -0400

    forms 2

--- Dj/MasterBikes/forms.py
+++ Dj/MasterBikes/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
+""" from .models import FormaPago """
 
 class CustomUserCreationForm(UserCreationForm):
     pass
+
+""" class GeneroForm(ModelForm):
+    class Meta:
+        model = Genero
+        fields = "__all__" """
