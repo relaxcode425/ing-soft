675173f74c1f058ef48aa9b59e5413cf5b91905b
relaxcode425 <jamesvergaracoronado@gmail.com>
2024-06-25 12:16:25 -0400

    a

--- Dj/MasterBikes/urls.py
+++ Dj/MasterBikes/urls.py
@@ -3,4 +3,5 @@ from MasterBikes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crud-varios', views.crud_varios, name='crud-varios'),
 ]
--- Dj/MasterBikes/views.py
+++ Dj/MasterBikes/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Usuario
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     context={}
     return render(request, 'pages/index.html', context)
+
+def crud_varios(request):
+    usuarios = Usuario.objects.all()
+    context = {
+        "usuarios": usuarios,
+    }
+    return render(request, "pages/crud/crud_varios.html", context)
